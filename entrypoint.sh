0ba80598cf384771f6c4066e2a537bf5f69a2bb4
Ira W. Snyder <isnyder@lco.global>
2020-03-31 12:32:41 -0700

    Implement default to run on every Puppet manifest individually

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,10 @@
 
 set -euo pipefail
 
-echo "ARGUMENTS: $@"
-exec /opt/puppetlabs/bin/puppet parser validate "$@"
+if [[ "$#" -eq "1" ]]; then
+	# No arguments given, run the syntax checker on every Puppet manifest in the current directory
+	/usr/bin/find . -iname '*.pp' -type f -print0 | xargs -0 /opt/puppetlabs/bin/puppet parser validate
+else
+	# Run the syntax checker on the given files / directories
+	exec /opt/puppetlabs/bin/puppet parser validate "$@"
+fi
